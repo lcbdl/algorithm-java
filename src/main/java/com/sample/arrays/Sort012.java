7a288c00e87b7bd2b25e291aa4aa74d7718a09b7
Chuanbao Lu <chuanbao.lu@gmail.com>
2024-09-16 23:38:22 +0000

    more code

--- src/main/java/com/sample/arrays/Sort012.java
+++ src/main/java/com/sample/arrays/Sort012.java
@@ -6,6 +6,23 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+/**
+ * Given an array arr containing only 0s, 1s, and 2s. Sort the array in
+ * ascending order.
+ * 
+ * Examples:
+ * 
+ * Input: arr[]= [0, 2, 1, 2, 0]
+ * Output: 0 0 1 2 2
+ * Explanation: 0s 1s and 2s are segregated into ascending order.
+ * 
+ * Input: arr[] = [0, 1, 0]
+ * Output: 0 0 1
+ * Explanation: 0s 1s and 2s are segregated into ascending order.
+ * 
+ * Expected Time Complexity: O(n)
+ * Expected Auxiliary Space: O(1)
+ */
 public class Sort012 {
   // Function to sort an array of 0s, 1s, and 2s
   public static void sort012(List<Integer> arr) {
